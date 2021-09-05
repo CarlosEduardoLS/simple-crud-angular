b1780b923538dac6194083ea9d126501499573d1
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-05 01:22:11 -0300

    FIX: Adjust archives in repository

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,40 @@
+import { Router, ActivatedRoute } from '@angular/router'
+import { ProductService } from './../product.service'
+import { Product } from './../product.model'
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css'],
+})
+export class ProductDeleteComponent implements OnInit {
+  product: Product = {
+    name: '',
+    price: 0,
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id: string = this.route.snapshot.paramMap.get('id')!
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  deleteProduct() {
+    this.productService.delete(`${this.product.id}`).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['/products'])
+  }
+}
